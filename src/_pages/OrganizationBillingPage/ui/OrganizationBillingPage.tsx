0bc04dafebeccdf1a982d137175553f32808115d
Vadim <119458044+VadimGalian@users.noreply.github>
2024-04-06 10:27:12 +0300

    int

--- src/_pages/OrganizationBillingPage/ui/OrganizationBillingPage.tsx
+++ src/_pages/OrganizationBillingPage/ui/OrganizationBillingPage.tsx
@@ -1,7 +1,7 @@
 import { checkSubscription } from "@/_entities/User"
 import { Separator } from "@/_shared/ui/Separator"
 import { OrganizationInfoCard } from "@/_entities/Organization"
-import { RedirectToStripeButton } from "@/_features/RedirectToStripe"
+import { RedirectToStripeButton } from "@/_features/RredirectToStripe"
 
 export async function OrganizationBillingPage() {
     const isPro = await checkSubscription()
