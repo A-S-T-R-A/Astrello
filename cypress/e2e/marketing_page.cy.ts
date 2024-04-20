adf90ceae2175f766c5f376ec3e63cc328abf491
jesuitdelray <jesuitdelray@gmail.com>
2024-04-20 15:46:28 +0300

    inter

--- cypress/e2e/marketing_page.cy.ts
+++ cypress/e2e/marketing_page.cy.ts
@@ -1,9 +1,9 @@
-describe("Marketing Page Tests", () => {
-    it('redirects to the sign-up page when clicking "Get Astrello for free"', () => {
-        cy.visit("http://localhost:3000/", {
-            failOnStatusCode: false,
-        })
-        cy.contains("a", "Get Astrello for free").click()
-        cy.url().should("include", "/sign-up")
-    })
-})
+describe("Login Page Tests", () => {
+  it('redirects to the sign-in page when clicking "Login"', () => {
+    cy.visit("http://localhost:3000/", {
+      failOnStatusCode: false
+    });
+    cy.contains("a", "Login").click();
+    cy.url().should("include", "/sign-in");
+  });
+});
